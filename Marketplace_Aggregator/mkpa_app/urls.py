860342caa89bc88f5a14b721c76dd6583926d361
rrdesh10 <51774394+rrdesh10@users.noreply.github.com>
2023-10-09 11:18:35 +0530

    stripe payments working

--- Marketplace_Aggregator/mkpa_app/urls.py
+++ Marketplace_Aggregator/mkpa_app/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('product/<int:id>', views.detail, name='detail'),
     path('success/', views.payment_success_view, name='success'),
     path('failed/', views.payment_failed_view, name='failed'),
-    path('api/checkout-session/<int:id>', views.create_checkout_session, name='api_checkout_session'),
+    path('api/checkoutsession/<int:id>', views.create_checkout_session, name='api_checkout_session'),
 ]
