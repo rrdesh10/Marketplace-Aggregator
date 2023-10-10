db75fe31a847e3f5abf56eeac0298410d7e9402c
rrdesh10 <51774394+rrdesh10@users.noreply.github.com>
2023-10-10 20:45:00 +0530

    Payments

--- Marketplace_Aggregator/mkpa_app/models.py
+++ Marketplace_Aggregator/mkpa_app/models.py
@@ -15,7 +15,7 @@ class OrderDetail(models.Model):
     customer_email = models.EmailField()
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     amount = models.IntegerField()
-    stripe_payment_intent = models.CharField(max_length=50)
+    stripe_payment_intent = models.CharField(max_length=100)
     has_paid = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now_add=True)
--- Marketplace_Aggregator/mkpa_app/urls.py
+++ Marketplace_Aggregator/mkpa_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('product/<int:id>', views.detail, name='detail'),
-    path('success/', views.payment_success_view, name='success'),
-    path('failed/', views.payment_failed_view, name='failed'),
+    path('done/', views.payment_success_view, name='done'),
+    path('failed', views.payment_failed_view, name='failed'),
     path('api/checkoutsession/<int:id>', views.create_checkout_session, name='api_checkout_session'),
 ]
