acb0d10550847d9c52c5c5351c80dfb28e426c12
rrdesh10 <51774394+rrdesh10@users.noreply.github.com>
2023-10-11 15:39:31 +0530

    Edit, delete func added

--- Marketplace_Aggregator/mkpa_app/urls.py
+++ Marketplace_Aggregator/mkpa_app/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('done/', views.payment_success_view, name='done'),
     path('failed', views.payment_failed_view, name='failed'),
     path('api/checkoutsession/<int:id>', views.create_checkout_session, name='api_checkout_session'),
+    path('create', views.create_product, name='create'),
+    path('edit/<int:id>', views.edit_product, name='edit'),
+    path('delete/<int:id>', views.delete_product, name='delete'),
 ]
